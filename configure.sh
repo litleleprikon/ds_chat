771941a94898fc6553e9723481fe2f25083d18fd
Emil Sharifullin <litleleprikon@gmail.com>
2016-12-09 13:14:47 +0300

    final

---
+++ configure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if [[ "$(docker images -q litleleprikon/front 2> /dev/null)" == "" ]]; then
+	printf "${YELLOW}Docker image litleleprikon/front is not found\nBuilding it.${NC}\n"
+	pushd front
+	rm -rf build
+	curl https://github.com/litleleprikon/ds_chat/releases/download/alpha/build.tar.gz
+	tar -xzf build.tar.gz
+	docker build -t litleleprikon/front .
+	popd
+fi
+
+printf "Front: [${GREEN}OK${NC}]\n"
+
+if  [[ "${?}" != "0" ]]; then
+	printf "${YELLOW}Docker image litleleprikon/back is not found\nBuilding it.${NC}\n"
+	pushd back
+	docker build -t litleleprikon/back .
+	popd
+fi
+
+printf "Back: [${GREEN}OK${NC}]\n"
