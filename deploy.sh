0f04f14c98a819429965b072ad296d063640b8f9
mrZizik <mrfreezzzer@mail.ru>
2017-04-23 21:22:10 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,6 @@ rm -rf ds_chat
 git clone https://github.com/mrZizik/ds_chat
 printf "Cloning: [${GREEN}OK${NC}]\n"
 
-printf "${YELLOW}Docker image lia/back is building.${NC}\n"
-cd ds_chat/back
-docker stop back || true
-docker rm back || true
-docker rmi back || true
-docker build -t back .
-docker run -d --name back -p 3001:3001 back
-printf "Back: [${GREEN}OK${NC}]\n"
-
 printf "${YELLOW}Docker image front is building.${NC}\n"
 cd ../front
 docker stop front || true
@@ -28,4 +19,15 @@ docker rmi front || true
 docker build -t front .
 docker run -d --name front -p 80:80 front
 printf "Front: [${GREEN}OK${NC}]\n"
+
+printf "${YELLOW}Docker image back is building.${NC}\n"
+cd ds_chat/back
+docker stop back || true
+docker rm back || true
+docker rmi back || true
+docker build -t back .
+docker run -d --name back -p 3001:3001 back
+printf "Back: [${GREEN}OK${NC}]\n"
+
+
 
