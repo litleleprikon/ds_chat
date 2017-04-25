138eeb0ae2041fe59acfd7b7dcf2fad9ea3389f6
mrZizik <mrfreezzzer@mail.ru>
2017-04-25 21:26:24 +0300

    check

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,6 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-printf "${YELLOW}Cheking code Sonar${NC}\n"
-ssh a_abdulmadzhidov@35.190.169.10 << EOF
-rm -rf ds_chat
-git clone https://github.com/mrZizik/ds_chat
-cd ds_chat
-/etc/sonarscan/bin/sonar-scanner
-printf "Cheking: [${GREEN}OK${NC}]\n"
-exit
-
 printf "${YELLOW}Cloning code from https://github.com/mrZizik/ds_chat${NC}\n"
 rm -rf ds_chat
 git clone https://github.com/mrZizik/ds_chat
