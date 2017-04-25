8745f7cf0de0a3e9b526df11831247a9f9f0c653
mrZizik <mrfreezzzer@mail.ru>
2017-04-25 18:40:10 +0300

    build

--- deploy.sh
+++ deploy.sh
@@ -32,7 +32,7 @@ git clone https://github.com/mrZizik/ds_chat
 
 sudo curl -o /usr/local/bin/docker-compose -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose up -d
+docker-compose up
 
 
 
