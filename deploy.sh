ff03250bd5b242bfe6517ef1b7d6b8b5b60e271d
mrZizik <mrfreezzzer@mail.ru>
2017-04-25 18:28:39 +0300

    build

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,6 @@ printf "Cloning: [${GREEN}OK${NC}]\n"
 
 printf "${YELLOW}Docker image back is building.${NC}\n"
 cd ds_chat/back
-docker stop back || true
-docker rm --force back || true
-docker rmi --force mrzizik/back || true
 docker build -t mrzizik/back .
 docker push mrzizik/back
 printf "Back: [${GREEN}OK${NC}]\n"
@@ -22,9 +19,6 @@ printf "Back: [${GREEN}OK${NC}]\n"
 
 printf "${YELLOW}Docker image front is building.${NC}\n"
 cd ../front
-docker stop front || true
-docker rm --force front || true
-docker rmi --force mrzizik/front || true
 docker build -t mrzizik/front .
 docker push mrzizik/front
 printf "Front: [${GREEN}OK${NC}]\n"
@@ -33,9 +27,13 @@ printf "${YELLOW}Pushing docker compose.${NC}\n"
 cd ..
 scp docker-compose.yml a_abdulmadzhidov@35.187.162.188:/home/a_abdulmadzhidov/docker-compose.yml
 ssh a_abdulmadzhidov@35.187.162.188 << EOF
+rm -rf ds_chat
+git clone https://github.com/mrZizik/ds_chat
+cd ds_chat
+
 sudo curl -o /usr/local/bin/docker-compose -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose up
+docker-compose up -d
 
 
 
